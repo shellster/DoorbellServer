ae12c38fd2bbce5f74ddd5e2da621d0861b6e77d
shellster <shellsterdude@gmail.com>
2016-06-16 15:21:54 -0600

    Added Pro Camera App Launch and minor cleanups

--- app/src/main/java/com/shellster/doorbellserver/udpServer.java
+++ app/src/main/java/com/shellster/doorbellserver/udpServer.java
@@ -1,22 +1,32 @@
 package com.shellster.doorbellserver;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 
 public class udpServer extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String[] params) {
-        PackageManager pm = watchDog.getAppContext().getPackageManager();
-        Intent intent = pm.getLaunchIntentForPackage("com.rcreations.ipcamviewerBasic");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        watchDog.getAppContext().startActivity(intent);
+        try {
+            PackageManager pm = watchDog.getAppContext().getPackageManager();
+            Intent intent = pm.getLaunchIntentForPackage("com.rcreations.ipcamviewerBasic");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            watchDog.getAppContext().startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PackageManager pm = watchDog.getAppContext().getPackageManager();
+            Intent intent = pm.getLaunchIntentForPackage("com.rcreations.WebCamViewerPaid");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            watchDog.getAppContext().startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         udpServerRun();
         return "Done";
@@ -38,10 +48,6 @@ public class udpServer extends AsyncTask<String, Void, String> {
                 lText = new String(lMsg, 0, dp.getLength());
                 if(lText.trim().equals("doorbell"))
                     mediaPlayer.start();
-            } catch (SocketException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
